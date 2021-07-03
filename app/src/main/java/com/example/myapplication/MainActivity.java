fdbcc76cf34c5d7be4e2edb86bd2dd9743fb504c
Jonathan Yang <shenaoyang@gmail.com>
2021-07-02 17:33:20 -0700

    Added MakePost Button; Created layout of the PostEntryDialog. It will dismiss if the user clicks "Cancel" or "Post"

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton btnMakePost = (FloatingActionButton) findViewById(R.id.btnMakePost);
+        if (btnMakePost == null) throw new AssertionError();
+        btnMakePost.setOnClickListener(view -> showDialog());
+    }
+
+    private void showDialog(){
+        PostEntryDialog dialog = new PostEntryDialog();
+        dialog.show(getSupportFragmentManager(),"DIALOG_FRAGMENT");
     }
 }
