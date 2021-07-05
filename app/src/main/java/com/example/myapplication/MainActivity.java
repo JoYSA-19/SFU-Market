1657cadd33d6582c8d07d532be8414fdfb34b9c5
Jonathan Yang <shenaoyang@gmail.com>
2021-07-04 18:27:40 -0700

    Added Camera button, the camera is working but the photo does not appear in the imageView. I think it is caused by the deprecated code on line 123.
    I followed this video:
    https://youtu.be/CYRXXOM3aGI
    The information about that deprecated code:
    https://stackoverflow.com/questions/62671106/onactivityresult-method-is-deprecated-what-is-the-alternative

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,27 +1,42 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private Retrofit retrofit;
     private RetrofitInterface retrofitInterface;
     private String BASE_URL = "http://10.0.2.2:3000";
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +68,9 @@ public class MainActivity extends AppCompatActivity {
 
         //assigning text fields and buttons to a variable
         Button postBtn = view.findViewById(R.id.btnPost);
+        ImageButton cameraBtn = view.findViewById(R.id.cameraButton);
+        ImageButton uploadBtn = view.findViewById(R.id.uploadImageButton);
+
         String itemName = view.findViewById(R.id.textName).toString();
         String itemDescription = view.findViewById(R.id.textDescription).toString();
         String textContact = view.findViewById(R.id.textContact).toString();
@@ -87,5 +105,39 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        cameraBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String fileName = "photo";
+                File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+                try {
+                    File imageFile = File.createTempFile(fileName,".jpg",storageDirectory);
+                    currentPhotoPath = imageFile.getAbsolutePath();
+
+                    Uri imageUri = FileProvider.getUriForFile(MainActivity.this,
+                            "com.example.myapplication.fileprovider",imageFile);
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+
+                    startActivityForResult(intent, 123);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
+
+            ImageView imageView = findViewById(R.id.imageView);
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
